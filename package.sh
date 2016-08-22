b864f9a16bf2896ad5615600e1f12b3bb72b2a31
Daniel Lemire <lemire@gmail.com>
2016-08-22 14:34:28 -0400

    Minor cleaning.

--- package.sh
+++
@@ -1,3 +0,0 @@
-cd ..;zip -9 -v ./externalsortinginjava/externalsortinginjava-0.1.1-src.zip ./externalsortinginjava/src/test/java/com/google/code/externalsorting/ExternalSortTest.java ./externalsortinginjava/src/main/java/com/google/code/externalsorting/ExternalSort.java ./externalsortinginjava/pom.xml ./externalsortinginjava/README.txt ./externalsortinginjava/CHANGELOG.txt ./externalsortinginjava/LICENSE.txt
-cd externalsortinginjava 
-mvn install
